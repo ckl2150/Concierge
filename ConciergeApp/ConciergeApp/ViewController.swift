aff74aaa575c3946f23281caec42a60ad02c4264
ckl2150 <conrad.liu21@gmail.com>
2017-11-13 01:18:00 -0500

    SHA256 FINALLY WORKS in viewcontroller

--- ConciergeApp/ConciergeApp/ViewController.swift
+++ ConciergeApp/ConciergeApp/ViewController.swift
@@ -42,10 +42,8 @@ class ViewController: UIViewController {
             else {
                 //SignUp
                 Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!, completion: { (user, error) in
-                    let email: String = self.emailText.text!
                     if user != nil {
-                        self.ref.child("user").setValue(email)
-                        self.ref.child(email).setValue("hello?")
+                        self.updateDB()
 //                        ref!.child("user/\(self.emailText.text)/email").setValue([self.emailText.text])
 //                        ref.child("user\(email)").setValue(self.passwordText.text)
                         // success
@@ -73,6 +71,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateDB() {
+        let hashedData: NSData = sha256(data: emailText.text!.data(using: String.Encoding.utf8)! as NSData)
+        let hashedEmail: String = hexStringFromData(input: sha256(data: hashedData))
+        ref.child("users").child(hashedEmail).setValue(["username": emailText.text!, "password": passwordText.text!])
+    }
+    
+    func sha256(data : NSData) -> NSData {
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
+        let res = NSData(bytes: hash, length: Int(CC_SHA256_DIGEST_LENGTH))
+        return res
+    }
+    
+    private func hexStringFromData(input: NSData) -> String {
+        var bytes = [UInt8](repeating: 0, count: input.length)
+        input.getBytes(&bytes, length: input.length)
+        
+        var hexString = ""
+        for byte in bytes {
+            hexString += String(format:"%02x", UInt8(byte))
+        }
+        
+        return hexString
+    }
 
 }
 
